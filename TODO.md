101dff09714fb7348e80dd213cf81f6c37999acd
Liam Stanley <me@liamstanley.io>
2023-09-05 00:27:01 -0400

    initial test logic
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- TODO.md
+++ TODO.md
@@ -1,4 +1,7 @@
 # TODO
 
-- error wrapping
+- finish error wrapping
 - look at --print-json
+- test removing flags to make sure they are actually removed
+- test cloning
+- generate tests which add, then immediately remove flags?
---
+++ command_test.go
@@ -0,0 +1,96 @@
+// Copyright (c) Liam Stanley <me@liamstanley.io>. All rights reserved. Use
+// of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+//nolint:forbidigo
+package ytdlp
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+	"testing"
+	"time"
+)
+
+type testSampleFile struct {
+	url       string
+	name      string
+	ext       string
+	extractor string
+}
+
+var sampleFiles = []testSampleFile{
+	{url: "https://cdn.liam.sh/github/go-ytdlp/sample-1.mp4", name: "sample-1", ext: "mp4", extractor: "generic"},
+	{url: "https://cdn.liam.sh/github/go-ytdlp/sample-2.mp4", name: "sample-2", ext: "mp4", extractor: "generic"},
+	{url: "https://cdn.liam.sh/github/go-ytdlp/sample-3.mp4", name: "sample-3", ext: "mp4", extractor: "generic"},
+	{url: "https://cdn.liam.sh/github/go-ytdlp/sample-4.mpg", name: "sample-4", ext: "mpg", extractor: "generic"},
+}
+
+func TestCommandSimple(t *testing.T) {
+	dir := t.TempDir()
+
+	var urls []string
+
+	for _, f := range sampleFiles {
+		urls = append(urls, f.url)
+	}
+
+	res, err := New().
+		Verbose().NoProgress().NoOverwrites().Output(filepath.Join(dir, "%(extractor)s - %(title)s.%(ext)s")).
+		Run(context.Background(), urls...)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res == nil {
+		t.Fatal("res is nil")
+	}
+
+	if res.ExitCode != 0 {
+		t.Fatalf("expected exit code 0, got %d", res.ExitCode)
+	}
+
+	if !slices.Contains(res.Args, "--verbose") {
+		t.Fatal("expected --verbose flag to be set")
+	}
+
+	for _, f := range sampleFiles {
+		t.Run(f.name, func(t *testing.T) {
+			var stat fs.FileInfo
+
+			stat, err = os.Stat(filepath.Join(dir, fmt.Sprintf("%s - %s.%s", f.extractor, f.name, f.ext)))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if stat.Size() == 0 {
+				t.Fatal("file is empty")
+			}
+		})
+	}
+}
+
+func TestCommandVersion(t *testing.T) {
+	res, err := New().Version(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res == nil {
+		t.Fatal("res is nil")
+	}
+
+	if res.ExitCode != 0 {
+		t.Fatalf("expected exit code 0, got %d", res.ExitCode)
+	}
+
+	_, err = time.Parse("2006.01.02", strings.TrimSuffix(res.Stdout, "\n"))
+	if err != nil {
+		t.Fatalf("failed to parse version: %v", err)
+	}
+}
