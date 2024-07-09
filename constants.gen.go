534d082b449cfa2676329cae8d534144002f34aa
Liam Stanley <liam@liam.sh>
2024-07-08 23:34:02 -0400

    update yt-dlp from 2024.07.08 to 2024.07.09 (#24)
    Co-authored-by: lrstanley <lrstanley@users.noreply.github.com>

--- constants.gen.go
+++ constants.gen.go
@@ -11,7 +11,7 @@ const (
 	Channel = "stable"
 
 	// Version of yt-dlp that go-ytdlp was generated with.
-	Version = "2024.07.08"
+	Version = "2024.07.09"
 )
 
 // Extractor contains information about a specific yt-dlp extractor. Extractors are
