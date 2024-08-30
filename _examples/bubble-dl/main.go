5a93a15c1c4859b494a7047d6db60a9fdc8bdade
Liam Stanley <liam@liam.sh>
2024-08-30 02:31:43 -0400

    add bubble-dl example
    Signed-off-by: Liam Stanley <liam@liam.sh>

---
+++ _examples/bubble-dl/main.go
@@ -0,0 +1,212 @@
+// Copyright (c) Liam Stanley <liam@liam.sh>. All rights reserved. Use of
+// this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/dustin/go-humanize"
+	"github.com/lrstanley/go-ytdlp"
+)
+
+const slowDownload = true
+
+var core *tea.Program
+
+var defaultDownloads = []string{
+	"https://cdn.liam.sh/github/go-ytdlp/sample-1.mp4",
+	"https://cdn.liam.sh/github/go-ytdlp/sample-2.mp4",
+	"https://cdn.liam.sh/github/go-ytdlp/sample-3.mp4",
+	"https://cdn.liam.sh/github/go-ytdlp/sample-4.mpg",
+}
+
+type model struct {
+	urls     []string
+	width    int
+	height   int
+	spinner  spinner.Model
+	progress progress.Model
+	done     bool
+
+	lastProgress ytdlp.ProgressUpdate
+}
+
+var (
+	fileStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
+	titleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("93"))
+	doneStyle    = lipgloss.NewStyle().Margin(1, 2)
+	errorStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("196")).SetString("✗")
+	successStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("42")).SetString("✓")
+	etaStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	sizeStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+)
+
+func newModel() model {
+	m := model{
+		spinner: spinner.New(),
+		progress: progress.New(
+			progress.WithDefaultGradient(),
+			progress.WithWidth(40),
+		),
+	}
+
+	if len(os.Args[1:]) > 0 {
+		m.urls = os.Args[1:]
+	} else {
+		m.urls = defaultDownloads
+	}
+
+	for _, uri := range m.urls {
+		_, err := url.Parse(uri)
+		if err != nil {
+			fmt.Printf("%s unvalid URL specified %q: %s\n", errorStyle, uri, err) //nolint:forbidigo
+			os.Exit(1)
+		}
+	}
+
+	m.spinner.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("63"))
+
+	return m
+}
+
+type MsgInstalled struct{}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		func() tea.Msg {
+			ytdlp.MustInstall(context.TODO(), nil)
+			return MsgInstalled{}
+		},
+		m.spinner.Tick,
+	)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width, m.height = msg.Width, msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc", "q":
+			return m, tea.Quit
+		}
+	case MsgInstalled:
+		return m, m.initiateDownload
+	case MsgProgress:
+		m.lastProgress = msg.Progress
+		cmds := []tea.Cmd{m.progress.SetPercent(msg.Progress.Percent() / 100)}
+
+		if m.lastProgress.Status == ytdlp.ProgressStatusFinished {
+			cmds = append(cmds, tea.Printf(
+				"%s downloaded %s (%s)",
+				successStyle,
+				fileStyle.Render(*m.lastProgress.Info.URL),
+				titleStyle.Render(*m.lastProgress.Info.Filename),
+			))
+		}
+		if m.lastProgress.Status == ytdlp.ProgressStatusError {
+			cmds = append(cmds, tea.Printf(
+				"%s error downloading: %s",
+				errorStyle,
+				*m.lastProgress.Info.URL,
+			))
+		}
+		return m, tea.Sequence(cmds...)
+	case MsgFinished:
+		m.done = true
+		if msg.Err != nil {
+			return m, tea.Sequence(
+				tea.Printf("%s error downloading urls: %s", errorStyle, msg.Err),
+				tea.Quit,
+			)
+		}
+		return m, tea.Quit
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	case progress.FrameMsg:
+		newModel, cmd := m.progress.Update(msg)
+		if newModel, ok := newModel.(progress.Model); ok {
+			m.progress = newModel
+		}
+		return m, cmd
+	}
+	return m, nil
+}
+
+type MsgProgress struct {
+	Progress ytdlp.ProgressUpdate
+}
+
+type MsgFinished struct {
+	Result *ytdlp.Result
+	Err    error
+}
+
+func (m model) initiateDownload() tea.Msg {
+	dl := ytdlp.New().
+		FormatSort("res,ext:mp4:m4a").
+		RecodeVideo("mp4").
+		ForceOverwrites().
+		ProgressFunc(100*time.Millisecond, func(prog ytdlp.ProgressUpdate) {
+			core.Send(MsgProgress{Progress: prog})
+		}).
+		Output("%(extractor)s - %(title)s.%(ext)s")
+
+	if slowDownload {
+		dl = dl.LimitRate("2M")
+	}
+
+	result, err := dl.Run(context.TODO(), m.urls...)
+
+	return MsgFinished{Result: result, Err: err}
+}
+
+func (m model) View() string {
+	// " <spinner> <status> <file> <GAP> <progress> [eta: <eta>] [size: <size>]"
+
+	if m.lastProgress.Status == "" {
+		return doneStyle.Render(m.spinner.View() + " fetching url information...\n")
+	}
+
+	if m.done {
+		return doneStyle.Render(fmt.Sprintf("downloaded %d urls.\n", len(m.urls)))
+	}
+
+	spin := m.spinner.View()
+	status := string(m.lastProgress.Status)
+	prog := m.progress.View()
+	eta := m.lastProgress.ETA().Round(time.Second).String()
+	eta = "[eta: " + etaStyle.MarginLeft(max(0, 4-len(eta))).Render(eta) + "]"
+	size := "[size: " + sizeStyle.Render(humanize.Bytes(uint64(m.lastProgress.TotalBytes))) + "]"
+
+	cellsAvail := max(0, m.width-lipgloss.Width(spin+" "+status+" "+prog+" "+eta+" "+size))
+
+	file := fileStyle.MaxWidth(cellsAvail).Render(m.lastProgress.Filename)
+
+	cellsRemaining := max(0, cellsAvail-lipgloss.Width(file))
+	gap := strings.Repeat(" ", cellsRemaining)
+
+	return spin + " " + status + " " + file + gap + prog + " " + eta + " " + size
+}
+
+func main() {
+	core = tea.NewProgram(newModel())
+	_, err := core.Run()
+	if err != nil {
+		fmt.Printf("error running program: %v\n", err) //nolint:forbidigo
+		os.Exit(1)
+	}
+}
---
+++ _examples/go.mod
@@ -0,0 +1,38 @@
+module github.com/lrstanley/go-ytdlp/_examples
+
+go 1.23.0
+
+replace github.com/lrstanley/go-ytdlp => ../
+
+require (
+	github.com/charmbracelet/bubbles v0.19.0
+	github.com/charmbracelet/bubbletea v1.0.1
+	github.com/charmbracelet/lipgloss v0.13.0
+	github.com/lrstanley/go-ytdlp v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/ProtonMail/go-crypto v1.0.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/harmonica v0.2.0 // indirect
+	github.com/charmbracelet/x/ansi v0.1.4 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
