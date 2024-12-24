ee89385d91e205e136525470830a255c36469f1f
lrstanley <1847365+lrstanley@users.noreply.github.com>
2024-12-24 00:19:14 +0000

    chore(deps): update yt-dlp from 2024.12.13 to 2024.12.23
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- constants.gen.go
+++ constants.gen.go
@@ -11,7 +11,7 @@ const (
 	Channel = "stable"
 
 	// Version of yt-dlp that go-ytdlp was generated with.
-	Version = "2024.12.13"
+	Version = "2024.12.23"
 )
 
 // Extractor contains information about a specific yt-dlp extractor. Extractors are
