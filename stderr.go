bf621362056f6ca3587972e216b7267d10ff6f08
FlameFlag <github@flameflag.dev>
2026-03-14 22:55:49 +0200

    chore: change license credit to lrstanley
    Co-authored-by: Liam Stanley <liam@liam.sh>
    Signed-off-by: FlameFlag <github@flameflag.dev>

--- stderr.go
+++ stderr.go
@@ -1,6 +1,6 @@
-// Copyright (c) FlameFlag <github@flameflag.dev>. All rights reserved. Use
-// of this source code is governed by the MIT license that can be found in the
-// LICENSE file.
+// Copyright (c) Liam Stanley <liam@liam.sh>. All rights reserved. Use of
+// this source code is governed by the MIT license that can be found in
+// the LICENSE file.
 
 package ytdlp
 
